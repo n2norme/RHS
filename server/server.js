e897a0ef4b98f997849887082570f293de42b64b
Nicolas Denorme <nicolas.denorme@icloud.com>
2020-11-17 17:04:24 +0100

    First development of the API.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
-const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
-const url = 'mongodb://localhost:27017/RHS';
 
-// Connection to Database
+// Connect Database : 
 
-mongoose.connect(url, {useNewUrlParser: true});
-
-
+connectDB();
 
 app.get('/', (req,res) => {res.send('API running')});
+
+// Init Middleware : 
+
+app.use(express.json({extended:false}));
+
+// Define Routes : 
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 
 
